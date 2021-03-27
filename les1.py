dee17c81c0d162e608a00e238e4634b5d794bcdd
alllnkas <staalyo0404@gmail.com>
2021-03-27 10:56:16 +0000

    первый урок

---
+++ les1.py
@@ -0,0 +1,60 @@
+#print(3)
+
+#print(3+4)
+
+#print(3, 4)
+
+#print(4**3) #возведение в степень
+
+#print(4//3) #целая часть от деления
+
+#type(3)
+
+#print(type(3))
+
+#print(type(3.2))
+
+#print(type("молодец"))
+
+#a=["molodec", 1, 6, 7]
+#print(type(a))
+
+#print(type(True))
+
+#print(type(False))
+
+#a=input()
+#print(a)
+
+#a=input("Введите значение a: ")
+#print(a)
+#print(type(a))
+
+#a="Good"
+#b="Bad"
+
+#print(a+b)#сложение строк
+#print(a*3)# умножение на числа
+
+#c=50
+#d=10
+#print(f"Вставим числа {c} и {d}") #f - строки
+
+#name=input("Как тебя зовут?")
+#print(f"Привет {name}")
+
+#age=int(input("Сколько тебе лет?"))
+#print(f'до столетия осталось {100-age}')
+
+#a= [1, 3, 6]
+#print(a[0])
+
+#b=[2, 4, 7]
+#c=a+b
+#print(c)
+
+s=[]
+s.append(10)
+print(s)
+
+print(len(a)) # команда определения длины списка
